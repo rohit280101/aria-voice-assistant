e62eea4f5c8f98209052114705635de2a9e0bea7
Rohit Gupta <guptarohitr68n@gmail.com>
2026-05-13 18:04:42 +0200

    "fix ssl for render postgres"

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 pool.on('error', (err) => console.error('DB pool error:', err))
 
